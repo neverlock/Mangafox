0db5dca401023a0d64584f5dc0fe7961857104f4
neverlock <neverlock@gmail.com>
2015-04-26 19:52:36 +0700

    add feature can download only 1 chapter

--- thai-cartoon.sh
+++ thai-cartoon.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
-if [ -z $2 ]
+if [ -z $2 ] && [ -z $3 ]
 then
 	echo "use $0 [manga name] [maxchapter]"
 	echo "example $0 read-inu-yashiki-chapter 28"
+	echo "use $0 [manga name] [chapter] 0 \nthat mean download only chapter"
+	echo "example $0 read-inu-yashiki-chapter 28 0"
 else
+	if [ ! -z $3 ]
+	then
+		echo "Build dir for save file $1/$2"
+		mkdir -p $1/$2
+		echo "Loading page... [$2]"
+		wget --quiet http://www.thai-cartoon.net/$1-$2.html -O volume.html
+		cat volume.html |grep "</Script>'"|awk -F"'" '{print $2}'>img.txt
+		COUNT=1
+		cat img.txt|while read LINE
+		do
+			echo "Loading... image $COUNT.jpg in [$2]"
+			wget --quiet $LINE -O $1/$2/$COUNT.jpg
+			COUNT=`expr $COUNT + 1`
+		done
+	rm volume.html
+	rm img.txt
+	exit
+	fi
+
+	if [ ! -z $2 ]
+	then
 	echo "Build dir for save file $1"
 	for ((volume=1 ; volume<=$2 ;volume++))
 	do
@@ -22,4 +45,8 @@ else
 	done
 	rm volume.html
 	rm img.txt
+	exit
+	fi
+
+
 fi
